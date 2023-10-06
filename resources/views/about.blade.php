c0faedc49e7173e9b6acb29918d8960a60263281
HubertSimonW <n00210409@iadt.ie>
2023-10-06 10:19:11 +0100

    Hubert

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welcome to my Laravel Home xD</d>
+
+                <div class="card-body">
+                    <p> This is the simple home view for my Laravel project.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
